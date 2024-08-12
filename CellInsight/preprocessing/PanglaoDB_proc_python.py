384b02c5e0e893a22507e7a982620c2486665e57
youngyoung <lhayoung030425@gmail.com>
2024-08-12 15:34:46 +0900

    preprocessing

--- CellInsight/preprocessing/PanglaoDB_proc_python.py
+++ CellInsight/preprocessing/PanglaoDB_proc_python.py
@@ -20,7 +20,14 @@ pandas2ri.activate()
 ro.r.source('PanglaoDB_proc_R.R')
 process_panglaodb = ro.r['process_panglaodb']
 
-def process_PanglaoDB(data_path):
+def process_PanglaoDB(data_path, additional_data):
+    '''
+    data_path : 
+    additional_data : list consisting of SRA, SRS, SRR, Species, Tumor, Protocol, Instrument, Full-length mRNA-seq, Number of cells,
+                      Number of exp. genes, Number of clusters, Tissue, Cell line (Y/N), Primary adult tissue (Y/N), and Target cell population
+    
+    '''
+    
     object = process_panglaodb(data_path)
     
     counts_matrix = np.array(object.rx2('counts_matrix'))
@@ -32,5 +39,17 @@ def process_PanglaoDB(data_path):
     processed_data = anndata.AnnData(X=counts_matrix)
     processed_data.obs['cell_ids'] = col_data_df
     processed_data.var['gene_ids'] = row_data_df
+    
+    processed_data.obs['species__ontology_label'] = additional_data['Species']
+    processed_data.obs['library_preparation_protocol__ontology_label'] = additional_data['Protocol']
+    processed_data.obs['organ__ontology_label'] = additional_data['Tissue']
+    processed_data.obs['tumor'] = additional_data['Tumor']
+    processed_data.obs['cell_line'] = additional_data['Cell line']
+    processed_data.obs['primary_adult_tissue'] = additional_data['Primary adult tissue']
+    
+    processed_data.uns['SRA'] = additional_data['SRA']
+    processed_data.uns['SRS'] = additional_data['SRS']
+    processed_data.uns['SRR'] = additional_data['SRR']
+    processed_data.uns['instrument'] = additional_data['Instrument']
     
     return processed_data
